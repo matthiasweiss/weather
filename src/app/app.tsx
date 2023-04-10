0f93ac46ed3d12d87472cf992b5d97036bdfd1fe
Matthias Weiß <mail@matthiasweiss.at>
2023-04-10 21:59:49 +0200

    Working fetch

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import { useEffect, useState } from 'react';
+import { components } from './api';
+
+type Weather = components['schemas']['weather'];
 
 export function App() {
+  const [date, setDate] = useState<string | undefined>(undefined);
+  const [weather, setWeather] = useState<Weather | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      if (!date) {
+        return;
+      }
+
+      const url = `http://localhost:3000/weather/${date}`;
+
+      const response = await fetch(url);
+      const weather = (await response.json()) as Weather;
+
+      setWeather(weather);
+    })();
+  }, [date]);
+
   return (
-    <>
-      <span className="bg-slate-500">HEllo</span>
-      <span>HEllo</span>
-    </>
+    <div className="flex w-full justify-center p-4">
+      <div className="flex items-center gap-4">
+        <h2 className="text-lg">Please select a date:</h2>
+        <input type="date" onChange={(e) => setDate(e.target.value)} />
+      </div>
+      {JSON.stringify(weather)}
+    </div>
   );
 }
 
