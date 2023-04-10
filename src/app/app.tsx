c69f34375145ee6d689477ae905ff74dd95eeb68
Matthias Weiß <mail@matthiasweiss.at>
2023-04-10 14:17:08 +0200

    Initial setup

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <>
-      <NxWelcome title="weather" />
-
-      <div />
+      <span className="bg-slate-500">HEllo</span>
+      <span>HEllo</span>
     </>
   );
 }
